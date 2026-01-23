92864aca073ea8046439d8335dcfafbbba97438f
showgo-nakajima <ebatech341@gmail.com>
2026-01-23 17:05:01 +0900

    ラジオボタンで税率を切り替え、表示できるように修正

--- Atached_Tex.js
+++ Atached_Tex.js
@@ -1,6 +1,7 @@
 /**
- * Kintoneアプリで、フィールドコード「単価」を対象に、
- * 数値を入力し、フォーカスアウトしたタイミングで、 税率を加算する機能。
+ * 1.Kintoneアプリで、フィールドコード「単価」を対象に、
+ *   税区分＋単価を入力し、フォーカスアウトしたタイミングで、 税率を加算する機能。
+ * 2.ラジオボタンで税率を切り替え、表示できるように修正。
  * 
  * 税率は10%
  */
@@ -10,10 +11,16 @@
 
   kintone.events.on(
     [
+      // 単価が変わった時のイベント
       'app.record.create.change.単価',
-      'app.record.edit.change.単価'
+      'app.record.edit.change.単価',
+
+      // 税率が変わった時のイベント
+      'app.record.create.change.税区分',
+      'app.record.edit.change.税区分'
     ],
     function (event) {
+
       const record = event.record;
 
       const basePrice = Number(record.単価.value);
@@ -24,7 +31,7 @@
       // 税率取得
       const TAXRATE = record.税区分.value === '8%' ? 0.08 : 0.10;
 
-      // 税込み計算（1回だけ）
+      // 税込み価格を再計算
       record.税込み価格.value = Math.round(
         basePrice * (1 + TAXRATE)
       );
