9aae65fb8352fd18176e413bf3dcb8ab4a7b7e29
showgo-nakajima <ebatech341@gmail.com>
2026-01-23 15:59:57 +0900

    即時関数化

--- Atached_Tex.js
+++ Atached_Tex.js
@@ -5,28 +5,31 @@
  * 税率は10%
  */
 
-kintone.events.on(
- [
- 'app.record.create.change.単価',
- 'app.record.edit.change.単価'
- ],
- function (event) {
- // on change
- const record = event.record;
- const taxRate = 0.1;
+(function () {
+  'use strict';
+  kintone.events.on(
+    [
+      'app.record.create.change.単価',
+      'app.record.edit.change.単価'
+    ],
+    function (event) {
+      // on change
+      const record = event.record;
+      const taxRate = 0.1;
 
- const price = record.単価.value;
+      const price = record.単価.value;
 
- // 未入力・0・NaN対策
- if (!price) {
-  return event;
- }
+      // 未入力・0・NaN対策
+      if (!price) {
+        return event;
+      }
 
- // 税込み計算
- const taxtedPrice = Math.round(price * taxRate);
+      // 税込み計算
+      const taxtedPrice = Math.round(price * taxRate);
 
- record.単価.value = taxtedPrice;
+      record.単価.value = taxtedPrice;
 
- return event;
-  }
-);
+      return event;
+    }
+  );
+})();
